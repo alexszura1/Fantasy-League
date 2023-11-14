2ec904d6c71e212200e09282dbce933e6cad281b
Alexander Szura <134403839+alexszura1@users.noreply.github.com>
2023-11-14 13:25:47 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,9 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
+  
   <p>Rat Behavior</p>
+`;
 
 /*   STEP 3   */
 /*
